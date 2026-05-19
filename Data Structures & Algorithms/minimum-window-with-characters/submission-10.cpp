940c05725a09f39ae958ea9b639f556ebe510a29
Kohav Dey <deykohav@gmail.com>
2026-05-19 12:11:35 +0530

    Add: minimum-window-with-characters - submission-10

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-10.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> u, x;
+        int Ns = s.size(), Nt = t.size(), m = INT_MAX;
+
+        string z;
+
+        if (Ns < Nt) return "";
+
+        for (auto const& i: t) ++u[i];
+
+        int l = 0, r = 0, have = 0, need = u.size();
+
+        while (r < Ns){
+            ++x[s[r]];
+
+            if (u.count(s[r]) && u[s[r]] == x[s[r]]) ++have;
+
+            while (have == need){
+                int k = r - l + 1;
+                if (k < m){
+                    m = k;
+                    z = s.substr(l, k);
+                }
+
+                --x[s[l]];
+                if (u.count(s[l]) && x[s[l]] < u[s[l]]) --have;
+                ++l;
+            }
+
+            ++r;
+        }
+
+        return z;
+    }
+};
