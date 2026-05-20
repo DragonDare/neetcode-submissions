29d37fc5bb90173c9662c8db431505ee9ea69923
Kohav Dey <deykohav@gmail.com>
2026-05-20 18:04:44 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> ss;
+
+        for (auto const& i: s){
+            if (i == '(' || i == '[' || i == '{') ss.push(i);
+            else {
+                if (ss.empty()) return false;
+                if (i == ')'){
+                    if (ss.top() != '(') return false;
+                    else ss.pop();
+                }
+                if (i == ']'){
+                    if (ss.top() != '[') return false;
+                    else ss.pop();
+                }
+                if (i == '}'){
+                    if (ss.top() != '{') return false;
+                    else ss.pop();
+                }
+            }
+        }
+
+        return ss.empty();
+    }
+};
